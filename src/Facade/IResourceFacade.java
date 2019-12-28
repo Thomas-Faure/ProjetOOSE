17fb219222202751cfb47224c76ffa1205ed0998
GAzerty <guilherme.t14@gmail.com>
2019-12-28 10:42:26 +0100

    Facade resource implementation, addRessource method in comment

--- src/Facade/IResourceFacade.java
+++ src/Facade/IResourceFacade.java
@@ -1,4 +1,8 @@
 package Facade;
 
+import BuisnessLogic.Ressource.Resource;
+
 public interface IResourceFacade {
+
+    //boolean addResource(Resource resource);
 }
--- src/Facade/ResourceFacade.java
+++ src/Facade/ResourceFacade.java
@@ -1,9 +1,42 @@
 package Facade;
 
-import BuisnessLogic.Ressource.AbstractResource;
+import BuisnessLogic.Ressource.*;
 import DAO.AbstractDAOFactory;
+import DAO.MySQLDAOFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResourceFacade implements IResourceFacade {
-    private AbstractResource[] resources;
+    private List<AbstractResource> resources;
     private AbstractDAOFactory daoFactory;
+
+    public static ResourceFacade FacadeInstance;
+    private ResourceFacade(){
+        //daoFactory = MySQLDAOFactory.getResourceDAO();
+        this.resources = new ArrayList<>();
+    }
+
+    public static ResourceFacade getInstance(){
+        if(FacadeInstance == null){
+            FacadeInstance = new ResourceFacade();
+        }
+        return FacadeInstance;
+    }
+
+    public AbstractDAOFactory getDao(){
+        return this.daoFactory;
+    }
+    public List<AbstractResource> getListResource(){
+        return this.resources;
+    }
+
+    /*public boolean addResource(Resource resource){
+         if(FacadeInstance.getDao().save(resource)){
+             FacadeInstance.getListResource().add(resource);
+            return true;
+        }else {
+            return false;
+        }
+    }*/
 }
