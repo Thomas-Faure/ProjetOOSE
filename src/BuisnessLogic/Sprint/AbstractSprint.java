74dd0769884b7884b44b0436580bea26905b79d2
GAzerty <guilherme.t14@gmail.com>
2019-12-28 22:55:11 +0100

    AbstractSprint and Sprint implemented

--- src/BuisnessLogic/Sprint/AbstractSprint.java
+++ src/BuisnessLogic/Sprint/AbstractSprint.java
@@ -1,4 +1,30 @@
 package BuisnessLogic.Sprint;
 
+import BuisnessLogic.Task.Task;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public abstract class AbstractSprint {
+
+    String sprintName;
+    LocalDate beginDate;
+    LocalDate endDate;
+    List<Task> taskList;
+
+    public abstract String getSprintName();
+
+    public abstract void setSprintName(String sprintName);
+
+    public abstract LocalDate getBeginDate();
+
+    public abstract void setBeginDate(LocalDate beginDate);
+
+    public abstract LocalDate getEndDate();
+
+    public abstract void setEndDate(LocalDate endDate);
+
+    public abstract List<Task> getTaskList();
+
+    public abstract void setTaskList(List<Task> taskList);
 }
--- src/BuisnessLogic/Sprint/Sprint.java
+++ src/BuisnessLogic/Sprint/Sprint.java
@@ -1,12 +1,61 @@
 package BuisnessLogic.Sprint;
 
-import Facade.Task.ITaskFacade;
+import BuisnessLogic.Task.Task;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sprint extends AbstractSprint {
-    private String name;
-    private Date beginDate;
-    private Date endDate;
-    private ITaskFacade tasks;
+    String sprintName;
+    LocalDate beginDate;
+    LocalDate endDate;
+    List<Task> taskList;
+
+    public Sprint(String sprintName, LocalDate beginDate, LocalDate endDate) {
+        this.sprintName = sprintName;
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+        this.taskList = new ArrayList<Task>();
+    }
+
+    @Override
+    public String getSprintName() {
+        return this.sprintName;
+    }
+
+    @Override
+    public void setSprintName(String sprintName) {
+        this.sprintName = sprintName;
+    }
+
+    @Override
+    public LocalDate getBeginDate() {
+        return this.beginDate;
+    }
+
+    @Override
+    public void setBeginDate(LocalDate beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    @Override
+    public LocalDate getEndDate() {
+        return this.endDate;
+    }
+
+    @Override
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public List<Task> getTaskList() {
+        return this.taskList;
+    }
+
+    @Override
+    public void setTaskList(List<Task> taskList) {
+        this.taskList = taskList;
+    }
 }
