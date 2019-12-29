84d6c900e34d3cf5e55eae3b7a316b30f87a5a76
GAzerty <guilherme.t14@gmail.com>
2019-12-29 12:06:00 +0100

    ListView initialized in RessourceUI

--- src/Controller/Resource/ResourceController.java
+++ src/Controller/Resource/ResourceController.java
@@ -1,21 +1,34 @@
 package Controller.Resource;
 
+import BuisnessLogic.Announcement.AbstractAnnouncement;
 import BuisnessLogic.Announcement.Announcement;
 import BuisnessLogic.Ressource.AbstractResource;
 import BuisnessLogic.Ressource.Resource;
+import Controller.Announcement.AnnouncementController;
+import Facade.Announcement.AnnouncementFacade;
 import Facade.IResourceFacade;
 import Facade.ResourceFacade;
 import Main.App;
+import UI.Announcement.UIModifyAnnouncement;
+import UI.Announcement.UIReadAnnouncement;
 import UI.Ressource.ResourceUI;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+
+import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import java.io.File;
 
@@ -33,17 +46,31 @@ public class ResourceController implements Initializable {
     private Button buttonSearch;
 
     @FXML
-    private ListView<Announcement> fileList;
+    private ListView<AbstractResource> resourceList;
+
+    private static ObservableList<AbstractResource> listViewTemp;
 
     private int projectID;
 
+    private DropBoxConnexion dropboxPPM;
+
     public ResourceController(int projectID){
         this.projectID = projectID;
+        this.dropboxPPM = new DropBoxConnexion();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        if(resourceList != null){
 
+            List<AbstractResource> listeElement = ResourceFacade.getInstance().getListResourceByProject(1);
+            System.out.println("listeElement size :"+listeElement.size());
+            ObservableList<AbstractResource> listView = FXCollections.observableArrayList(listeElement);
+            listViewTemp = FXCollections.observableArrayList(listeElement);
+            resourceList.setItems(listView);
+            resourceList.setCellFactory(param -> new Cell());
+            System.out.println(resourceList.getItems());
+        }
     }
 
     @FXML
@@ -57,7 +84,7 @@ public class ResourceController implements Initializable {
         System.out.println(filename);
 
         System.out.println("ID DU PROJET COURANT: "+projectID);
-        DropBoxConnexion dropboxPPM = new DropBoxConnexion();
+
 
         //Project p = ProjectFacade.getInstance().getProjectById();
         //
@@ -67,5 +94,49 @@ public class ResourceController implements Initializable {
 
         AbstractResource resource = new Resource(dropboxpath_file,filename);
         ResourceFacade.getInstance().addResource(resource);
+    }
+
+
+    static class Cell extends ListCell<AbstractResource> {
+        AbstractResource resource;
+        HBox hbox = new HBox();
+        Button btnDelete = new Button("Delete");
+        Button btnDownload = new Button("Download");
+        Label label = new Label("");
+        Pane pane = new Pane();
+        public Cell(){
+            super();
+            hbox.setSpacing(10);
+            hbox.getChildren().addAll(label,pane,btnDownload,btnDelete);
+            btnDelete.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+
+                    System.out.println("Supprimer le fichier !!");
+                }
+            });
+
+            btnDownload.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+
+                    System.out.println("Télécharger le fichier !!");
+                }
+            });
+
+
+        }
+        @Override
+        public void updateItem(AbstractResource name, boolean empty){
+            super.updateItem(name,empty);
+            setText(null);
+            setGraphic(null);
+            if(name != null && !empty){
+                resource = name;
+                label.setText(name.getFilename());
+                setGraphic(hbox);
+            }
+        }
+
     }
 }
