1a6da23b6d0ab03df1d26f21b4b16ce80b93fd24
GAzerty <guilherme.t14@gmail.com>
2019-12-30 12:04:05 +0100

    Cell and initialize done

--- src/Controller/Sprint/SprintController.java
+++ src/Controller/Sprint/SprintController.java
@@ -2,15 +2,26 @@ package Controller.Sprint;
 
 import BuisnessLogic.Ressource.AbstractResource;
 import BuisnessLogic.Sprint.AbstractSprint;
+import Controller.Resource.DropBoxConnexion;
+import Controller.Resource.ResourceController;
 import Facade.ISprintFacade;
+import Facade.ResourceFacade;
+import Facade.SprintFacade;
+import Main.App;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class SprintController implements Initializable {
@@ -35,6 +46,46 @@ public class SprintController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        if(sprintList != null){
+
+            List<AbstractSprint> listeElement = SprintFacade.getInstance().getListSprintByProject(projectID);
+            ObservableList<AbstractSprint> listView = FXCollections.observableArrayList(listeElement);
+            listViewTemp = FXCollections.observableArrayList(listeElement);
+            sprintList.setItems(listView);
+            sprintList.setCellFactory(param -> new SprintController.Cell());
+        }
+    }
+
+    static class Cell extends ListCell<AbstractSprint> {
+        AbstractSprint sprint;
+        HBox hbox = new HBox();
+        Button btnRead = new Button("Read");
+        Label label = new Label("");
+        Pane pane = new Pane();
+
+        public Cell(){
+            super();
+            hbox.setSpacing(10);
+            hbox.getChildren().addAll(label,pane,btnRead);
+            btnRead.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+                    System.out.println("See sprint !! OK");
+                }
+            });
+
+        }
+        @Override
+        public void updateItem(AbstractSprint name, boolean empty){
+            super.updateItem(name,empty);
+            setText(null);
+            setGraphic(null);
+            if(name != null && !empty){
+                sprint = name;
+                label.setText(name.getSprintName());
+                setGraphic(hbox);
+            }
+        }
 
     }
 }
