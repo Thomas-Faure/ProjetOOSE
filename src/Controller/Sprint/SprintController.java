6a74b3624546b914d973c83814af56cf3d05f7c8
GAzerty <guilherme.t14@gmail.com>
2019-12-30 10:55:28 +0100

    SprintController created

--- src/Controller/Sprint/SprintController.java
+++ src/Controller/Sprint/SprintController.java
@@ -1,14 +1,40 @@
-package Controller;
+package Controller.Sprint;
 
+import BuisnessLogic.Ressource.AbstractResource;
+import BuisnessLogic.Sprint.AbstractSprint;
 import Facade.ISprintFacade;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
-public class SprintController {
-    public static SprintController instance;
-    private ISprintFacade sFacade;
-    public static SprintController getInstance(){
-        if(instance == null){
-            instance= new SprintController();
-        }
-        return instance;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SprintController implements Initializable {
+
+    @FXML
+    private Button buttonCreate;
+
+    @FXML
+    private ListView<AbstractSprint> sprintList;
+
+    private static ObservableList<AbstractSprint> listViewTemp;
+
+    private int projectID;
+
+
+    /*public static SprintController instance;
+    private ISprintFacade sFacade;*/
+
+    public SprintController(int projectID){
+        this.projectID = projectID;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
