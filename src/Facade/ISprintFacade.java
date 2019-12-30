1030e736eea95c9a867777d07bb4332b17983bbb
GAzerty <guilherme.t14@gmail.com>
2019-12-30 12:00:21 +0100

    SprintFacade done, use of getListSprintByProject

--- src/Facade/ISprintFacade.java
+++ src/Facade/ISprintFacade.java
@@ -1,4 +1,13 @@
 package Facade;
 
+import BuisnessLogic.Sprint.AbstractSprint;
+
+import java.util.List;
+
 public interface ISprintFacade {
+
+    boolean addSprint(AbstractSprint sprint);
+    boolean deleteSprint(int sprintID);
+    AbstractSprint getSprintById(int sprintID);
+    List<AbstractSprint> getListSprintByProject(int projectID);
 }
