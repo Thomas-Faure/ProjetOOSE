23ceb1c054a95201425409516ebae64925058d9e
GAzerty <guilherme.t14@gmail.com>
2020-01-01 22:00:49 +0100

    Back to sprintUI after action on button. Use of ModifySprintUI

--- src/Controller/Sprint/AddSprintController.java
+++ src/Controller/Sprint/AddSprintController.java
@@ -1,10 +1,16 @@
 package Controller.Sprint;
 
+import BuisnessLogic.Sprint.AbstractSprint;
+import BuisnessLogic.Sprint.Sprint;
+import Facade.SprintFacade;
+import Main.App;
+import UI.Sprint.SprintUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 import java.time.LocalDate;
 
@@ -25,13 +31,16 @@ public class AddSprintController {
 
     @FXML
     void saveSprint(ActionEvent actionEvent){
-        System.out.println(inputSprintName.getText());
         LocalDate localDateBegin = inputBeginDate.getValue();
         LocalDate localDateEnd = inputEndDate.getValue();
-        System.out.println(localDateBegin);
-        System.out.println(localDateEnd);
+        AbstractSprint newSprint = new Sprint(inputSprintName.getText(),localDateBegin,localDateEnd);
+        SprintFacade.getInstance().addSprint(newSprint,projectID);
 
-        System.out.println("SAVED !!!");
+        SprintUI sprintUI = new SprintUI(1);
+        HBox box = (HBox) App.getInstanceScene().lookup("#HBOX");
+        if(box.getChildren().size() >1 )
+            box.getChildren().remove(1);
+        box.getChildren().add(sprintUI.loadScene().getRoot());
     }
 
     private int projectID;
