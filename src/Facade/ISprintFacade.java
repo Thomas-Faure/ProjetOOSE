07707909f12cf8377ffc071dc51b23e9aa8ac305
GAzerty <guilherme.t14@gmail.com>
2020-01-01 22:05:40 +0100

    updateSprint method added, addSprint method updated

--- src/Facade/ISprintFacade.java
+++ src/Facade/ISprintFacade.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public interface ISprintFacade {
 
-    boolean addSprint(AbstractSprint sprint);
+    boolean addSprint(AbstractSprint sprint,int projectID);
+    boolean updateSprint(AbstractSprint sprint);
     boolean deleteSprint(int sprintID);
     AbstractSprint getSprintById(int sprintID);
     List<AbstractSprint> getListSprintByProject(int projectID);
