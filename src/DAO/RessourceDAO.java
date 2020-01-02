8364f4fc05342a5407df7cc816aa9e4a07d08dee
GAzerty <guilherme.t14@gmail.com>
2020-01-02 16:14:16 +0100

    intermediate table between resource and project

--- src/DAO/RessourceDAO.java
+++ src/DAO/RessourceDAO.java
@@ -5,7 +5,7 @@ import BuisnessLogic.Ressource.AbstractResource;
 import java.util.List;
 
 public interface RessourceDAO {
-    boolean save(AbstractResource resource);
+    boolean save(AbstractResource resource, int projectID);
     boolean update(AbstractResource resource);
     boolean delete(int resourceID);
     AbstractResource getResourceById(int resourceID);
--- src/DAO/RessourceDAOMySQL.java
+++ src/DAO/RessourceDAOMySQL.java
@@ -2,6 +2,7 @@ package DAO;
 
 import BuisnessLogic.Ressource.AbstractResource;
 import BuisnessLogic.Ressource.Resource;
+import BuisnessLogic.Sprint.AbstractSprint;
 import BuisnessLogic.Task.AbstractTask;
 import BuisnessLogic.Task.Task;
 import BuisnessLogic.Task.TaskState;
@@ -16,21 +17,60 @@ import java.util.List;
 public class RessourceDAOMySQL implements RessourceDAO {
 
     private static final String INSERT = "INSERT INTO resource (path, filename) VALUES (?, ?)";
+    private static final String INSERT_INTERMEDIATE_TABLE = "INSERT INTO resource_project (resourceID,projectID) VALUES(?,?)";
     private static final String UPDATE = "UPDATE resource SET path=?, filename=? WHERE resourceID=?";
     private static final String DELETE = "DELETE FROM resource WHERE resourceID=?";
     private static final String RESOURCEBYID = "SELECT * FROM resource where resourceID=?";
     private static final String RESOURCEBYPROJECT = "SELECT resourceID FROM resource_project WHERE projectID=?";
+    private static final String LASTID = "SELECT MAX(resourceID) AS \"maxresourceID\" FROM resource";
 
     public RessourceDAOMySQL(){
     }
 
     @Override
-    public boolean save(AbstractResource resource) {
+    public boolean save(AbstractResource resource, int projectID) {
         boolean success = false;
         try {
             PreparedStatement ps = MySQLConnector.getSQLConnection().prepareStatement(INSERT);
             ps.setString(1, resource.getPath());
             ps.setString(2, resource.getFilename());
+            int i = ps.executeUpdate();
+            ps.close();
+            if (i > 0) {
+                success = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        AbstractResource newResource = getLastResourceAdded();
+        success = saveIntermediateTable(newResource.getResourceID(),projectID);
+
+        return success;
+    }
+
+    public AbstractResource getLastResourceAdded(){
+        int lastResourceID = 0;
+        try {
+            PreparedStatement ps = MySQLConnector.getSQLConnection().prepareStatement(LASTID);
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                lastResourceID = rs.getInt("maxresourceID");
+            }
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return getResourceById(lastResourceID);
+    }
+
+    public boolean saveIntermediateTable(int resourceID,int projectID){
+
+        boolean success = false;
+        try {
+            PreparedStatement ps = MySQLConnector.getSQLConnection().prepareStatement(INSERT_INTERMEDIATE_TABLE);
+            ps.setInt(1, resourceID);
+            ps.setInt(2, projectID);
             int i = ps.executeUpdate();
             ps.close();
             if (i > 0) {
