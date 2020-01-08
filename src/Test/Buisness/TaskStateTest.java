658299ddfd8f55e46d1ed6be6f1945c32ccd55c5
thomas <thomas.faure@etu.univ-amu.fr>
2020-01-08 12:40:32 +0100

    implementation of tests

--- src/Test/Buisness/TaskStateTest.java
+++ src/Test/Buisness/TaskStateTest.java
@@ -1,16 +1,19 @@
 package Test.Buisness;
 
+import BuisnessLogic.Task.TaskState;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskStateTest {
-
     @Test
     void getStatetoString() {
+        TaskState testState = TaskState.getStateByString("todo");
+        String test = testState.getStatetoString();
+        assertEquals("todo",test,"");
     }
-
     @Test
     void getStateByString() {
+        assertEquals(TaskState.todo, TaskState.getStateByString("todo"),"");
     }
 }
