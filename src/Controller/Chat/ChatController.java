39cf948610114b04c6720d0eade461a4137f4e75
GAzerty <guilherme.t14@gmail.com>
2020-01-10 23:35:47 +0100

    Begin of ChatController

--- src/Controller/Chat/ChatController.java
+++ src/Controller/Chat/ChatController.java
@@ -1,19 +1,78 @@
-package Controller;
+package Controller.Chat;
 
 
+import BuisnessLogic.Project.AbstractProject;
+import Controller.IController;
 import Facade.IChatFacade;
+import Main.App;
+import UI.Project.ProjectUI;
+import UI.Project.ReadProjectUI;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.scene.control.ListView;
 
-public class ChatController {
-    static ChatController instance;
-    private ChatController(){
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    }
+
+public class ChatController implements Initializable, IController {
+
+    AbstractProject project;
     private IChatFacade cFacade;
 
-    public static ChatController getInstance(){
-        if(instance == null){
-            instance= new ChatController();
-        }
-        return instance;
+    @FXML
+    private Text titlepath;
+
+    @FXML
+    private TextArea messageTextArea;
+
+    @FXML
+    private ListView peopleView;
+
+    @FXML
+    private ListView messagesView;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button sendButton;
+
+
+    public ChatController(AbstractProject project){
+        this.project = project;
+    }
+
+
+    @FXML
+    void sendMessage(ActionEvent actionEvent){
+        System.out.println("MESSAGE: "+messageTextArea.getText());
+    }
+
+    @FXML
+    void backToProjectView(ActionEvent actionEvent){
+
+        ReadProjectUI readProjectUI = new ReadProjectUI(project);
+        HBox box = (HBox) App.getInstanceScene().lookup("#HBOX");
+        if(box.getChildren().size() >1 )
+            box.getChildren().remove(1);
+        box.getChildren().add(readProjectUI.loadScene().getRoot());
+    }
+
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        titlepath.setText("/Project"+project.getName()+"/Chat");
+        update();
     }
 }
