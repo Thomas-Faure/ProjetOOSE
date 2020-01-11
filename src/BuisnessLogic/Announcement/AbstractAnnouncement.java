a76181359ded53bc7ea00ecd639d5b24ceb28672
thomas <thomas.faure@etu.univ-amu.fr>
2020-01-11 21:31:54 +0100

    add javadoc's comments for Buisnesslogic

--- src/BuisnessLogic/Announcement/AbstractAnnouncement.java
+++ src/BuisnessLogic/Announcement/AbstractAnnouncement.java
@@ -1,22 +1,58 @@
 package BuisnessLogic.Announcement;
-/**
- *
- * @author Thomas Faure
- */
+
 import BuisnessLogic.User.AbstractUser;
 import BuisnessLogic.User.User;
 
 import java.time.LocalDate;
 
+/**
+ * The announcement class (here the abstract)
+ * @author ThomasFaure
+ */
 public abstract class AbstractAnnouncement {
 
+    /**Method to return the announcement id
+     * @return
+     */
     public abstract int getId();
+
+    /**Method to return the title of an announcement
+     * @return
+     */
     public abstract String getTitle();
+
+    /**Method to return the message of an announcement
+     * @return
+     */
     public abstract String getMessage();
+
+    /**Method to return the dateo of an announcement
+     * @return
+     */
     public abstract LocalDate getDate();
+
+    /**Method to return the creator of an announcement
+     * @return
+     */
     public abstract AbstractUser getUser();
+
+    /**Method to set the id of an announcement
+     * @param id
+     */
     public abstract void setId(int id);
+
+    /**Method to set the title of an announcement
+     * @param title
+     */
     public abstract void setTitle(String title);
+
+    /**Method to set the message of an announcement
+     * @param msg
+     */
     public abstract void setMessage(String msg);
+
+    /**Method to set the date of an announcement
+     * @param date
+     */
     public abstract void setDate(LocalDate date);
 }
--- src/BuisnessLogic/Task/AbstractTask.java
+++ src/BuisnessLogic/Task/AbstractTask.java
@@ -9,25 +9,106 @@ import BuisnessLogic.User.User;
 
 import java.time.LocalDate;
 
+/**
+ *
+ * The task class (here the abstract)
+ *
+ * @author Thomas Faure
+ */
 public abstract class AbstractTask {
+    /**Method to get the id of a task
+     * @return
+     */
     public abstract int getId();
+
+    /**Method to get the name of a task
+     * @return
+     */
     public abstract String getName();
+
+    /**Method to get the priority of a task
+     * @return
+     */
     public abstract int getPriority();
+
+    /**Method to get the deadline of a task
+     * @return
+     */
     public abstract LocalDate getDeadline();
+
+    /**Method to get the creator of a task
+     * @return
+     */
     public abstract AbstractUser getCreator();
+
+    /**Method to get the description of a task
+     * @return
+     */
     public abstract String getDescription();
+
+    /**Method to get the current state (to string) of a task
+     * @return
+     */
     public abstract  String getStateString();
+
+    /**Method to set the state of a task
+     * @param state
+     */
     public abstract void setState(TaskState state);
+
+    /**Method to get the state of a task
+     * @return
+     */
     public abstract TaskState getState();
+
+    /**Method to set the id of a task
+     * @param id
+     */
     public abstract void setId(int id);
+
+    /**Method to set the name of a staks
+     * @param name
+     */
     public abstract void setName(String name);
+
+    /**Method to set the priority of a task
+     * @param priority
+     */
     public abstract void setPriority(int priority);
+
+    /**Method to set the deadline of a task
+     * @param deadline
+     */
     public abstract void setDeadline(LocalDate deadline);
+
+    /**Method to set the creator of a task
+     * @param creator
+     */
     public abstract void setCreator(User creator);
+
+    /**Method to set the description of a task
+     * @param description
+     */
     public abstract void setDescription(String description);
+
+    /**Method to set the project of a task
+     * @param project
+     */
     public abstract void setProject(AbstractProject project);
+
+    /**Method to get the project of a task
+     * @return
+     */
     public abstract AbstractProject getProject();
+
+    /**Methof to get the sprint's id of a task
+     * @return
+     */
     public abstract Integer getIdSprint();
+
+    /**Method to set the sprint's id of a task
+     * @param id
+     */
     public abstract void setIdSprint(int id);
 
 
