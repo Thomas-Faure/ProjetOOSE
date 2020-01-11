976740ead39fdd76458e56303e29c5900738f5bc
GAzerty <guilherme.t14@gmail.com>
2020-01-11 15:24:17 +0100

    ChatFacade implemented

--- src/Facade/ChatFacade.java
+++ src/Facade/ChatFacade.java
@@ -1,9 +1,53 @@
 package Facade;
 
 import BuisnessLogic.Chat.AbstractChat;
+import BuisnessLogic.Chat.Message;
 import DAO.AbstractDAOFactory;
+import DAO.ChatDAO;
+import DAO.MySQLDAOFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatFacade implements IChatFacade {
-    private AbstractDAOFactory daoFactory;
+    private ChatDAO dao;
     private AbstractChat[] chats;
+    public static ChatFacade FacadeInstance;
+
+    private ChatFacade(){
+        this.dao = MySQLDAOFactory.getChatDAO();
+    }
+
+    public static ChatFacade getInstance(){
+        if(FacadeInstance == null){
+            FacadeInstance = new ChatFacade();
+        }
+        return FacadeInstance;
+    }
+
+    @Override
+    public boolean addChat(AbstractChat chat) {
+        return dao.save(chat);
+    }
+
+    @Override
+    public boolean deleteChat(AbstractChat chat) {
+        return dao.delete(chat.getIdChat());
+    }
+
+    @Override
+    public boolean updateChat(AbstractChat chat) {
+        return dao.update(chat);
+    }
+
+    @Override
+    public AbstractChat getChatByProjectId(int idProject) {
+        AbstractChat chat = dao.getChatByProjectId(idProject);
+
+        //FacadeMessage.getMessageByChat(idChat)
+        //List<Message> messageStored = new ArrayList<Message>();
+        //chat.setHistoriqueMessage(messageStored);
+
+        return chat;
+    }
 }
--- src/Facade/IChatFacade.java
+++ src/Facade/IChatFacade.java
@@ -1,4 +1,11 @@
 package Facade;
 
+import BuisnessLogic.Chat.AbstractChat;
+
 public interface IChatFacade {
+
+    boolean addChat(AbstractChat chat);
+    boolean deleteChat(AbstractChat chat);
+    boolean updateChat(AbstractChat chat);
+    AbstractChat getChatByProjectId(int idProject);
 }
